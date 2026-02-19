f1e54175eb90b25710d44d340fa703ef17c9d26e
Valentina <valentina.pericu@gmail.com>
2026-02-19 12:00:14 +0100

    [minor] change node name

--- go1_viz/launch/view_robot.launch.py
+++ go1_viz/launch/view_robot.launch.py
@@ -22,7 +22,7 @@ def generate_launch_description():
     rviz = Node(
             package='rviz2',
             executable='rviz2',
-            name='rviz_ocs2',
+            name='rviz2',
             output='screen',
             arguments=["-d", rviz_config_file]
         )
